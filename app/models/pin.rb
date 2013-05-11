a1a1b9c38dd19a70576efe5208336ff0dbc776cd
Chris <Burts88@gmail.com>
2013-05-11 02:30:35 -0500

    added paperclip fix

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,8 +4,8 @@ class Pin < ActiveRecord::Base
 
   validates :description, presence: true
   validates_attachment :image, presence: true,
-  														 content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-  														 size: { less_than: 5.megabytes }
+  							   content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+  							   size: { less_than: 5.megabytes }
   validates :user_id, presence: true
 
   belongs_to :user
